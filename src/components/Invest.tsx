5db147d3a37c65e6efb8e2bcca9de03d3bd01616
Maryammunshi3028 <maryamfaizan3028@gmail.com>
2025-12-15 15:48:16 +0300

    Update src/components/Invest.tsx with new Invest component

--- src/components/Invest.tsx
+++ src/components/Invest.tsx
@@ -15,7 +15,7 @@ import {
   Users,
 } from 'lucide-react';
 
-import Invest from "./components/Invest";
+export default function Invest() {
   const [selectedCategory, setSelectedCategory] = useState('all');
   const [hoveredCard, setHoveredCard] = useState<number | null>(null);
 
